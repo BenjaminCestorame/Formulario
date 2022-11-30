d2a1038cb5dc7e13918c1c2bc5ef489af6607d0c
benjamincestorame3 <Benjamincestorame@gmail.com>
2022-11-30 15:09:46 -0300

    primercomit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Contact from './indexsu';
+function App() {
+  return (
+   <Contact/>
+  );
+}
+
+export default App;
---
+++ src/indexsu.js
@@ -0,0 +1,38 @@
+import Form from "./Form"
+import {useState} from "react";
+import axios from "axios";
+
+const Contact = () =>{
+    const [user, setUser] = useState({nombre: "", lastName: "", mensaje: "", mail:""})
+    const[sucess, setSucess]= useState(false)
+    const[error, setError]= useState(false)
+
+     const handleChange = (event) =>{
+        setSucess(false)
+        setError(false)
+        const prop = event.target.name
+        const value = event.target.value
+        setUser({...user, [prop]: value}) //spread operator
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        // https://jsonplaceholder.typicode.com/todos/1
+        axios({
+            url: "https://benjaapi123.herokuapp.com/",
+            method: "POST",
+            data: user
+        }).then((response)=>{
+            setUser({nombre: "", lastName: "", mensaje: "", mail:""})
+            setSucess(true)
+        }).catch((response)=>{
+            setError(true)
+        
+        })
+    }
+    return(
+        <Form user={user} handleChange={handleChange} handleSubmit={handleSubmit} sucess={sucess} error={error}/>
+    )
+}
+
+export default Contact;
